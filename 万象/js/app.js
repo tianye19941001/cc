926672b1f7237f92d63343899b8040169b1e0086
tianye <838199892@qq.com>
2017-03-28 19:32:39 +0800

    1042

--- 万象/js/app.js
+++ 万象/js/app.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 	function autoheight(){
 		var Nwidth = $(window).width(),
 			Nheight = Nwidth/1920*800,
-			Nheighti = Nwidth/1920*1020;
+			Nheighti = Nwidth/1920*680;
 		if (Nwidth > 1280) {
 			$('.video_con,.wx_list_f').height(Nheight);
 			$('.wx_lunbo .bd li img').height(Nheighti);
 		}else{
 			$('.video_con,.wx_list_f').height(1280/1920*800);
-			$('.wx_lunbo .bd li img').height(1280/1920*1020);
+			$('.wx_lunbo .bd li img').height(1280/1920*680);
 		}
 	}
 	$('.wlIn li').height($('.wlIn li').width());
